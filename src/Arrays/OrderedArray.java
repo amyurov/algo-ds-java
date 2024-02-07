a6fb8acb974ded99d992295335b362a7fd91c344
Artem Yurov <amyurov.dev@mail.ru>
2024-02-07 13:50:30 +0300

    refactor simple array

--- src/Arrays/OrderedArray.java
+++ src/Arrays/OrderedArray.java
@@ -21,61 +21,53 @@ public class OrderedArray implements Array<Integer> {
                 elemCount++;
                 return;
             }
-            if (checkBound(value) != -1) {
-                int insertIndex = checkBound(value);
-                if (insertIndex == 0) {
-                    forwardShift(0);
-                    array[0] = value;
-                    elemCount++;
-                    return;
-                } else {
-                    array[insertIndex] =  value;
-                }
-            }
 
             int insertIndex = biSearch(value);
             if (array[insertIndex].equals(value)) {
                 System.out.println("doubles");
                 return;
+            }
+
+            if ((value > array[insertIndex]) && (insertIndex == elemCount - 1)) {
+                array[elemCount] = value;
             } else if (value < array[insertIndex]) {
-                forwardShift(insertIndex - 1);
-                array[insertIndex - 1] = value;
-                elemCount++;
+                forwardShift(insertIndex);
+                array[insertIndex] = value;
             } else {
                 forwardShift(insertIndex + 1);
                 array[insertIndex + 1] = value;
-                elemCount++;
             }
+            elemCount++;
+            return;
         }
         System.out.println("Array is full");
     }
 
     @Override
     public Integer find(Integer value) {
-        System.out.println("Finding value: " + value);
         int mostLikelyIndex = biSearch(value);
-        return array[mostLikelyIndex].equals(value) ? mostLikelyIndex : -1;
+        return array[mostLikelyIndex].equals(value) ? mostLikelyIndex : null;
     }
 
     @Override
     public void delete(Integer value) {
-        System.out.println("deleting value " + value);
-        int indexToDel = find(value);
-        if (indexToDel == -1) {
+        Integer indexToDel = find(value);
+        if (indexToDel == null) {
             System.out.println("Not found to del");
         } else {
             array[indexToDel] = null;
             backShift(indexToDel);
+            elemCount--;
         }
     }
 
-    private int biSearch(Integer value) {
+    public int biSearch(Integer value) {
         int low = 0;
         int high = elemCount - 1;
         int currIn = 0;
         while (low != high) {
             currIn = (low + high) / 2;
-            if (value.equals(array[currIn])) {
+            if (value.equals(array[currIn]) || currIn + 1 > elemCount || currIn - 1 < 0) {
                 return currIn;
             } else if (value > array[currIn]) {
                 low = ++currIn;
@@ -86,17 +78,7 @@ public class OrderedArray implements Array<Integer> {
         return currIn;
     }
 
-    private int checkBound(Integer value) {
-        if (value < array[0]) {
-            return 0;
-        } else if (value > array[elemCount - 1]) {
-            return elemCount;
-        }
-        return -1;
-    }
-
-    private void forwardShift(int insertIndex) {
-        System.out.println("Shifting forward to index " + insertIndex);
+    protected void forwardShift(int insertIndex) {
         for (int i = elemCount; i > insertIndex; i--) {
             array[i] = array[i - 1];
             array[i - 1] = null;
@@ -104,7 +86,6 @@ public class OrderedArray implements Array<Integer> {
     }
 
     protected void backShift(int indexToFill) {
-        System.out.println("Shifting back to index " + indexToFill);
         for (int i = indexToFill; i < elemCount - 1; i++) {
             array[i] = array[i + 1];
             array[i + 1] = null;
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,45 @@
+import Arrays.OrderedArray;
+import Arrays.SimpleArray;
+
 public class Main {
     public static void main(String[] args) {
+
+        int cap = 10;
+//        SimpleArray unordered = new SimpleArray(cap);
+//        SimpleArray ordered = new SimpleArray(cap);
+//
+//        unordered.insert(5);
+//        unordered.insert(3);
+//        unordered.insert(8);
+//        unordered.insert(6);
+//        unordered.insert(9);
+//        unordered.insert(2);
+//        unordered.insert(4);
+//        unordered.insert(1);
+//        unordered.insert(7);
+//        unordered.insert(0);
+//
+//        unordered.print();
+//
+//        for (int i = 0; i < cap; i++) {
+//            ordered.insert(unordered.removeMax());
+//        }
+//
+//        ordered.print();
+
+        OrderedArray orderedArray = new OrderedArray(cap);
+
+        orderedArray.insert(0);
+        orderedArray.insert(2);
+        orderedArray.insert(4);
+        orderedArray.insert(6);
+        orderedArray.insert(8);
+        orderedArray.insert(10);
+        orderedArray.insert(12);
+        orderedArray.insert(14);
+
+        System.out.println(orderedArray.biSearch(11));
+
 
     }
 }
